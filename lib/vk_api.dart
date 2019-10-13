3fd8eb084286e8e40b2f78e03339098b334e6d3c
Tarangok <tarangok@mail.ru>
2019-10-13 20:57:57 +0300

    Initial commit

---
+++ lib/vk_api.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library vk_api;
+
+export 'src/vk_api_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
